f1df05509672196a5c2a3754ff1e201a6a7e92c0
SzczepanRy <>
2023-12-12 15:44:56 +0100

    a

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { roomI, roomListI } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -6,13 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent {
+  rooms: roomI = {
+    totalRooms: 20,
+    availibleRooms: 1,
+    bookedRooms: 5,
+  };
+
+  roomList: roomListI[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenites: 'air conditioning ',
+      price: 500,
+      photos:
+        'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcf.bstatic.com%2Fxdata%2Fimages%2Fhotel%2Fmax1024x768%2F194702399.jpg%3Fk%3D14e9a332f4f20a67f72f8d5938cd6577f5c932a0653f59ae89d020f119f0d9be%26o%3D%26hp%3D1&tbnid=Dbw5564463BwEM&vet=12ahUKEwiDssa3-YmDAxWi5gIHHd_0A2sQMygCegQIARBQ..i&imgrefurl=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fcy%2Fthe-room.pl.html&docid=o5agkt7Lm2FHrM&w=1024&h=768&q=room&ved=2ahUKEwiDssa3-YmDAxWi5gIHHd_0A2sQMygCegQIARBQ',
+      checkInTime: new Date('11-nov-2023'),
+      checkOutTime: new Date('11-nov-2023'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenites: 'air conditioning ',
+      price: 500,
+      photos:
+        'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcf.bstatic.com%2Fxdata%2Fimages%2Fhotel%2Fmax1024x768%2F194702399.jpg%3Fk%3D14e9a332f4f20a67f72f8d5938cd6577f5c932a0653f59ae89d020f119f0d9be%26o%3D%26hp%3D1&tbnid=Dbw5564463BwEM&vet=12ahUKEwiDssa3-YmDAxWi5gIHHd_0A2sQMygCegQIARBQ..i&imgrefurl=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fcy%2Fthe-room.pl.html&docid=o5agkt7Lm2FHrM&w=1024&h=768&q=room&ved=2ahUKEwiDssa3-YmDAxWi5gIHHd_0A2sQMygCegQIARBQ',
+      checkInTime: new Date('11-nov-2023'),
+      checkOutTime: new Date('11-nov-2023'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Deluxe Room',
+      amenites: 'air conditioning ',
+      price: 500,
+      photos:
+        'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcf.bstatic.com%2Fxdata%2Fimages%2Fhotel%2Fmax1024x768%2F194702399.jpg%3Fk%3D14e9a332f4f20a67f72f8d5938cd6577f5c932a0653f59ae89d020f119f0d9be%26o%3D%26hp%3D1&tbnid=Dbw5564463BwEM&vet=12ahUKEwiDssa3-YmDAxWi5gIHHd_0A2sQMygCegQIARBQ..i&imgrefurl=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fcy%2Fthe-room.pl.html&docid=o5agkt7Lm2FHrM&w=1024&h=768&q=room&ved=2ahUKEwiDssa3-YmDAxWi5gIHHd_0A2sQMygCegQIARBQ',
+      checkInTime: new Date('11-nov-2023'),
+      checkOutTime: new Date('11-nov-2023'),
+    },
+  ];
+
   hotelName: string = 'hotel';
   numberOfRooms: number = 10;
   hideHotels = false;
+
   toggle() {
     this.hideHotels = !this.hideHotels;
   }
   add() {
-    this.numberOfRooms++;
+    this.rooms.availibleRooms++;
   }
 }
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,15 @@
+export interface roomI {
+  totalRooms: number;
+  availibleRooms: number;
+  bookedRooms: number;
+}
+
+export interface roomListI {
+  roomNumber: number;
+  roomType: string;
+  amenites: string;
+  price: number;
+  photos: string;
+  checkInTime: Date;
+  checkOutTime: Date;
+}
